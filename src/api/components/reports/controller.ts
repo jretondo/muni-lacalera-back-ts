61a9b74f6096744bb71e9a835534a386a4feedc0
jretondo <jretondo90@gmail.com>
2022-08-28 23:01:35 -0300

    fix bug

--- src/api/components/reports/controller.ts
+++ src/api/components/reports/controller.ts
@@ -68,58 +68,64 @@ export = (injectedStore: typeof StoreType) => {
         let newData: Array<newDataProv> = []
         let totalToPay = 0
 
-        return await new Promise((resolve, reject) => {
-            data.map(async (item, key) => {
-                const filter1: Array<IWhereParams> | undefined = [];
-                filter1.push({
-                    mode: EModeWhere.strict,
-                    concat: EConcatWhere.and,
-                    items: [
-                        { column: Columns.works.id_provider, object: String(item.id_provider) },
-                        { column: Columns.works.month, object: String(month) },
-                        { column: Columns.works.year, object: String(year) }
-                    ]
-                })
+        if (data.length > 0) {
+            return await new Promise((resolve, reject) => {
+                data.map(async (item, key) => {
+                    const filter1: Array<IWhereParams> | undefined = [];
+                    filter1.push({
+                        mode: EModeWhere.strict,
+                        concat: EConcatWhere.and,
+                        items: [
+                            { column: Columns.works.id_provider, object: String(item.id_provider) },
+                            { column: Columns.works.month, object: String(month) },
+                            { column: Columns.works.year, object: String(year) }
+                        ]
+                    })
 
-                const filter2: Array<IWhereParams> | undefined = [];
-                filter2.push({
-                    mode: EModeWhere.strict,
-                    concat: EConcatWhere.and,
-                    items: [
-                        { column: Columns.payment_details.id_provider, object: String(item.id_provider) },
-                        { column: Columns.payment_details.month, object: String(month) },
-                        { column: Columns.payment_details.year, object: String(year) }
-                    ]
-                })
-                const totalWorkQuery: Array<{
-                    totalWork: number
-                }> = await store.list(Tables.WORKS, [`SUM(${Columns.works.amount}) as totalWork`], filter1)
-                const totalPaymentQuery: Array<{
-                    totalPayment: number
-                }> = await store.list(Tables.PAYMENT_DETAILS, [`SUM(${Columns.payment_details.amount}) as totalPayment`], filter2)
+                    const filter2: Array<IWhereParams> | undefined = [];
+                    filter2.push({
+                        mode: EModeWhere.strict,
+                        concat: EConcatWhere.and,
+                        items: [
+                            { column: Columns.payment_details.id_provider, object: String(item.id_provider) },
+                            { column: Columns.payment_details.month, object: String(month) },
+                            { column: Columns.payment_details.year, object: String(year) }
+                        ]
+                    })
+                    const totalWorkQuery: Array<{
+                        totalWork: number
+                    }> = await store.list(Tables.WORKS, [`SUM(${Columns.works.amount}) as totalWork`], filter1)
+                    const totalPaymentQuery: Array<{
+                        totalPayment: number
+                    }> = await store.list(Tables.PAYMENT_DETAILS, [`SUM(${Columns.payment_details.amount}) as totalPayment`], filter2)
 
-                let workTotal = 0
-                let paymentTotal = 0
-                if ((totalWorkQuery[0].totalWork) > 0) {
-                    workTotal = totalWorkQuery[0].totalWork
-                }
-                if ((totalPaymentQuery[0].totalPayment) > 0) {
-                    paymentTotal = totalPaymentQuery[0].totalPayment
-                }
+                    let workTotal = 0
+                    let paymentTotal = 0
+                    if ((totalWorkQuery[0].totalWork) > 0) {
+                        workTotal = totalWorkQuery[0].totalWork
+                    }
+                    if ((totalPaymentQuery[0].totalPayment) > 0) {
+                        paymentTotal = totalPaymentQuery[0].totalPayment
+                    }
 
-                const difference = workTotal - paymentTotal
-                newData.push({
-                    ...item,
-                    totalWork: workTotal,
-                    totalPayment: paymentTotal,
-                    difference: difference
+                    const difference = workTotal - paymentTotal
+                    newData.push({
+                        ...item,
+                        totalWork: workTotal,
+                        totalPayment: paymentTotal,
+                        difference: difference
+                    })
+                    totalToPay = totalToPay + difference
+                    if (key === data.length - 1) {
+                        resolve({ data: newData, totalToPay, pagesObj })
+                    }
                 })
-                totalToPay = totalToPay + difference
-                if (key === data.length - 1) {
-                    resolve({ data: newData, totalToPay, pagesObj })
-                }
             })
-        })
+        } else {
+            return {
+                data: []
+            }
+        }
 
     }
 
@@ -185,32 +191,39 @@ export = (injectedStore: typeof StoreType) => {
         }
 
         let newDetails: Array<paymentsTotal> = []
-        return await new Promise((resolve, reject) => {
-            data.map(async (item, key) => {
-                const filters2: Array<IWhereParams> | undefined = [];
 
-                filters2.push({
-                    mode: EModeWhere.higherEqual,
-                    concat: EConcatWhere.and,
-                    items: [
-                        { column: `${Tables.PAYMENT_DETAILS}.${Columns.payment_details.month}`, object: String(month) },
-                        { column: `${Tables.PAYMENT_DETAILS}.${Columns.payment_details.year}`, object: String(year) },
-                        { column: Columns.payment_details.payment_id, object: String(item.payment_id) }
-                    ]
+        if (data.length > 0) {
+            return await new Promise((resolve, reject) => {
+                data.map(async (item, key) => {
+                    const filters2: Array<IWhereParams> | undefined = [];
+
+                    filters2.push({
+                        mode: EModeWhere.higherEqual,
+                        concat: EConcatWhere.and,
+                        items: [
+                            { column: `${Tables.PAYMENT_DETAILS}.${Columns.payment_details.month}`, object: String(month) },
+                            { column: `${Tables.PAYMENT_DETAILS}.${Columns.payment_details.year}`, object: String(year) },
+                            { column: Columns.payment_details.payment_id, object: String(item.payment_id) }
+                        ]
+                    })
+
+                    const data2: Array<IDetPayments> = await store.list(Tables.PAYMENT_DETAILS, [ESelectFunct.all], filters);
+
+                    newDetails.push({
+                        paymentData: item,
+                        installments: data2
+                    })
+                    if (key === data.length - 1) {
+                        resolve({ data: newDetails })
+                    }
+
                 })
-
-                const data2: Array<IDetPayments> = await store.list(Tables.PAYMENT_DETAILS, [ESelectFunct.all], filters);
-
-                newDetails.push({
-                    paymentData: item,
-                    installments: data2
-                })
-                if (key === data.length - 1) {
-                    resolve({ data: newDetails })
-                }
-
             })
-        })
+        } else {
+            return { data: [] }
+        }
+
+
     }
 
     return {
